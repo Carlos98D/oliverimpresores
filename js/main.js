577769990b1201e13fbadc0c0883225b7a235e8d
Carlos98D <127034836+Carlos98D@users.noreply.github.com>
2026-07-07 19:43:11 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+async function cargarComponente(idContenedor, archivoHTML) {
+    try {
+        const respuesta = await fetch(archivoHTML);
+        if (!respuesta.ok) throw new Error(`No se pudo cargar ${archivoHTML}`);
+        const html = await respuesta.text();
+        document.getElementById(idContenedor).innerHTML = html;
+    } catch (error) {
+        console.error("Error cargando el componente:", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    cargarComponente("global-header", "components/header.html");
+    cargarComponente("global-footer", "components/footer.html");
+});
