8a633bcc7bed3c37a0f3bd48c2ef2af198f5ff0e
Carlos98D <127034836+Carlos98D@users.noreply.github.com>
2026-07-13 11:56:16 -0600

    main.js

--- js/main.js
+++ js/main.js
@@ -14,22 +14,3 @@ document.addEventListener("DOMContentLoaded", () => {
     cargarComponente("global-footer", "components/footer.html");
 });
 
-
-// Dentro de tu archivo js/main.js
-document.addEventListener('DOMContentLoaded', () => {
-    const triggers = document.querySelectorAll('.faq-trigger');
-    
-    triggers.forEach(trigger => {
-        trigger.addEventListener('click', () => {
-            const item = trigger.parentElement;
-            
-            // Cierra otros acordeones para mantener el orden
-            document.querySelectorAll('.faq-item').forEach(i => {
-                if(i !== item) i.classList.remove('active');
-            });
-            
-            // Alterna el actual
-            item.classList.toggle('active');
-        });
-    });
-});
